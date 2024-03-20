1757d54d45a5e68e631ae0b855844929f5e8245a
stevecalla <callasteven@gmail.com>
2024-03-20 10:40:53 -0600

    Add create final table

--- create_pacing_data/sql_getKeyMetrics_loop.js
+++ create_pacing_data/sql_getKeyMetrics_loop.js
@@ -18,7 +18,7 @@ function createLocalConnection() {
     });
 }
 
-// Function to execute query for a single date range
+// Function to execute query for a each pickup_year_month
 async function executeInsertQuery(pool, pickup_month_year, index) {
     return new Promise((resolve, reject) => {
 
@@ -66,6 +66,42 @@ async function executeInsertQuery(pool, pickup_month_year, index) {
             }
         });
 
+        // const addCreateAtDate = `
+        // ALTER TABLE pacing_base_all_calendar_dates ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;`
+
+        // pool.query(addCreateAtDate, (queryError, results) => {
+        //     if (queryError) {
+        //         console.error('Error executing select query:', queryError);
+        //         reject(queryError);
+        //     } else {
+        //         console.log('\nCreate table results');
+        //         console.table(results);
+        //         console.log('Create table results\n');
+        //         resolve();
+        //     }
+        // });
+
+    });
+}
+
+// Function to execute query for a single date range
+async function executeInsertCreatedAtQuery(pool, table) {
+    return new Promise((resolve, reject) => {
+        const addCreateAtDate = `
+        ALTER TABLE ${table} ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;`
+        console.log(addCreateAtDate);
+
+        pool.query(addCreateAtDate, (queryError, results) => {
+            if (queryError) {
+                console.error('Error executing select query:', queryError);
+                reject(queryError);
+            } else {
+                console.log('\nCreate at insert results');
+                console.table(results);
+                console.log('Create at insert results\n');
+                resolve();
+            }
+        });
     });
 }
 
@@ -142,6 +178,98 @@ async function executeCreateTableQuery(pool, table) {
     });
 }
 
+async function executeCreateFinalDataQuery(pool) {
+    return new Promise((resolve, reject) => {
+            
+        const query = `CREATE TABLE pacing_final_data AS
+        SELECT
+            -- pickup_month_year
+            CASE
+                WHEN pickup_month_year IS NULL THEN (
+                    SELECT inner_table.pickup_month_year
+                    FROM pacing_base_all_calendar_dates AS inner_table
+                    WHERE inner_table.grouping_id = pacing_base_all_calendar_dates.grouping_id
+                    AND inner_table.booking_date < pacing_base_all_calendar_dates.booking_date
+                    AND inner_table.pickup_month_year IS NOT NULL
+                    ORDER BY inner_table.booking_date DESC
+                    LIMIT 1)
+                ELSE pickup_month_year
+                END AS pickup_month_year,
+            booking_date,
+            
+            -- days_from_first_day_of_month
+            CASE
+                WHEN first_day_of_month IS NULL THEN (
+                    SELECT DATEDIFF(pacing_base_all_calendar_dates.booking_date, STR_TO_DATE(inner_table.first_day_of_month  , '%Y-%m-%d'))
+                    FROM pacing_base_all_calendar_dates AS inner_table
+                    WHERE inner_table.grouping_id = pacing_base_all_calendar_dates.grouping_id
+                    AND inner_table.booking_date < pacing_base_all_calendar_dates.booking_date
+                    AND inner_table.first_day_of_month IS NOT NULL
+                    ORDER BY inner_table.booking_date DESC
+                    LIMIT 1)
+                ELSE days_from_first_day_of_month
+                END AS days_from_first_day_of_month,
+            
+            COALESCE(count, 0) AS count,
+            COALESCE(total_booking_charge_aed, 0) AS total_booking_charge_aed,
+            COALESCE(total_booking_charge_less_discount_aed, 0) AS total_booking_charge_less_discount_aed,
+            
+            -- running_count
+            CASE
+                WHEN running_total_booking_count IS NULL THEN (
+                    SELECT inner_table.running_total_booking_count
+                    FROM pacing_base_all_calendar_dates AS inner_table
+                    WHERE inner_table.grouping_id = pacing_base_all_calendar_dates.grouping_id
+                    AND inner_table.booking_date < pacing_base_all_calendar_dates.booking_date
+                    AND inner_table.running_total_booking_count IS NOT NULL
+                    ORDER BY inner_table.booking_date DESC
+                    LIMIT 1)
+                ELSE running_total_booking_count
+                END AS running_count,
+            
+            -- running_total_booking_charge_aed
+            CASE
+                WHEN running_total_booking_charge_aed IS NULL THEN (
+                    SELECT inner_table.running_total_booking_charge_aed
+                    FROM pacing_base_all_calendar_dates AS inner_table
+                    WHERE inner_table.grouping_id = pacing_base_all_calendar_dates.grouping_id
+                    AND inner_table.booking_date < pacing_base_all_calendar_dates.booking_date
+                    AND inner_table.running_total_booking_charge_aed IS NOT NULL
+                    ORDER BY inner_table.booking_date DESC
+                    LIMIT 1)
+                ELSE running_total_booking_charge_aed
+                END AS running_total_booking_charge_aed,
+            
+            -- running_total_booking_charge_less_discount_aed
+            CASE
+                WHEN running_total_booking_charge_less_discount_aed IS NULL THEN (
+                    SELECT inner_table.running_total_booking_charge_less_discount_aed
+                    FROM pacing_base_all_calendar_dates AS inner_table
+                    WHERE inner_table.grouping_id = pacing_base_all_calendar_dates.grouping_id
+                    AND inner_table.booking_date < pacing_base_all_calendar_dates.booking_date
+                    AND inner_table.running_total_booking_charge_less_discount_aed IS NOT NULL
+                    ORDER BY inner_table.booking_date DESC
+                    LIMIT 1)
+                ELSE running_total_booking_charge_less_discount_aed
+                END AS running_total_booking_charge_less_discount_aed
+                
+        FROM pacing_base_all_calendar_dates;
+        `;
+
+        pool.query(query, (queryError, results) => {
+            if (queryError) {
+                console.error('Error executing select query:', queryError);
+                reject(queryError);
+            } else {
+                console.log('\nCreate table results');
+                console.table(results);
+                console.log('Create table results\n');
+                resolve();
+            }
+        });
+    });
+}
+
 // Main function to handle SSH connection and execute queries
 async function main() { 
     try {
@@ -153,15 +281,11 @@ async function main() {
 
         //execute drop
         await executeDropTableQuery(pool, 'pacing_base_all_calendar_dates');
-        // await executeDropTableQuery(pool, 'pacing_final_data;');
         
         //execute create
         await executeCreateTableQuery(pool);
 
-        //execute insert
-        // await executeInsertQuery(pool, distinctList);
-        
-        // Execute queries for each date range
+        // Execute insert for pickup_month_year; use for loop to execute sequentially
         for (let i = 0; i < distinctList.length; i++) {
             const { pickup_month_year } = distinctList[i];
             const index = i + 1;
@@ -169,9 +293,14 @@ async function main() {
             console.log(`Query for ${pickup_month_year} executed successfully.`);
             generateLogFile('pacing_data', `Query for ${pickup_month_year} executed successfully.`, config.csvExportPath);
         }
-
-        //send distinct list to the execute function
-        // await executeQuery(pool, distinctList);
+    
+        // Insert created at data
+        await executeInsertCreatedAtQuery(pool, 'pacing_base_all_calendar_dates');
+        
+        // final table
+        await executeDropTableQuery(pool, 'pacing_final_data;');
+        await executeCreateFinalDataQuery(pool);
+        await executeInsertCreatedAtQuery(pool, 'pacing_final_data');
 
         // generateLogFile('onrent_data', `Query for ${startDate} to ${endDate} executed successfully.`, config.csvExportPath);
 
