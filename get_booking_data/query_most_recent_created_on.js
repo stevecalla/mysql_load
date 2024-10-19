236ec759b9935c015caa102b497942cc2be91801
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2024-10-18 22:29:10 -0600

    Update most recent query

--- get_booking_data/query_most_recent_created_on.js
+++ get_booking_data/query_most_recent_created_on.js
@@ -1,29 +1,54 @@
 const query_most_recent_create_on_date = `
     -- FINDS THE MOST RECENT CREATED RECORD AND UPDATED RECORD; INDICATES WHEN THE DB WAS LAST UPDATED
     SELECT 
-        last_updated, -- converts base time via MST + 7 to UTC (converts 10:07:53 to 16:07:53)
-
-        DATE_FORMAT(CONVERT_TZ(last_updated, '+00:00', '+00:00'), '%Y-%m-%d %H:%i:%s UTC') AS last_updated_utc,
-
         source_field, 
         
+        -- last updated dates
+        most_recent_event_update, -- converts base time via MST + 7 to UTC (converts 10:07:53 to 16:07:53)
+        DATE_FORMAT(CONVERT_TZ(most_recent_event_update, '+00:00', '+00:00'), '%Y-%m-%d %H:%i:%s UTC') AS most_recent_event_update_utc,
+
+        -- current timestamp dates
         CURRENT_TIMESTAMP AS execution_timestamp,
         DATE_FORMAT(CONVERT_TZ(CURRENT_TIMESTAMP, '+00:00', '+00:00'), '%Y-%m-%d %H:%i:%s UTC') AS execution_timestamp_utc,
-        TIMESTAMPDIFF(HOUR, last_updated, CURRENT_TIMESTAMP()) as time_stamp_difference,
+
+        -- variance between last updated and current timestamp
+        TIMESTAMPDIFF(HOUR, most_recent_event_update, CURRENT_TIMESTAMP()) as time_stamp_difference,
+    --     TIMESTAMPDIFF(HOUR, '2024-10-19 03:41:54', '2024-10-19 03:51:54') as time_stamp_difference_hour, -- in whole hours
+    --     TIMESTAMPDIFF(MINUTE, '2024-10-19 03:41:54', '2024-10-19 05:50:54') as time_stamp_difference_minutes, -- in minutes
         CASE
-            WHEN TIMESTAMPDIFF(HOUR, last_updated, CURRENT_TIMESTAMP()) <= 2 THEN "true"
+            WHEN TIMESTAMPDIFF(HOUR, most_recent_event_update, CURRENT_TIMESTAMP()) <= 2 THEN "true"
             ELSE "false"
-        END AS is_within_2_hours
+        END AS is_within_2_hours,
+        CASE
+            WHEN TIMESTAMPDIFF(MINUTE, most_recent_event_update, CURRENT_TIMESTAMP()) <= 15 THEN "true"
+            ELSE "false"
+        END AS is_within_15_minutes,
+        
+        most_recent_created_on,
+        most_recent_updated_on
 
     FROM (
-        SELECT MAX(created_on) AS last_updated, 'created_on' AS source_field, count(*) AS count
+            SELECT 
+                'created_on' AS source_field, 
+                MAX(created_on) AS most_recent_event_update, 
+                MAX(created_on) AS most_recent_created_on, 
+                MAX(updated_on) AS most_recent_updated_on,
+                count(*) AS count
             FROM myproject.rental_car_booking2
+
         UNION ALL
-        SELECT MAX(updated_on) AS last_updated, 'updated_on' AS source_field, count(*) AS count
+
+            SELECT 
+                'updated_on' AS source_field, 
+                MAX(updated_on) AS most_recent_event_update, 
+                MAX(created_on) AS most_recent_created_on, 
+                MAX(updated_on) AS most_recent_updated_on, 
+                count(*) AS count
             FROM myproject.rental_car_booking2
-    ) AS last_updated_table
-    ORDER BY last_updated DESC
-    LIMIT 1;
+
+        ) AS last_updated_table
+    ORDER BY most_recent_event_update DESC
+    LIMIT 1; -- keeps most recent created on record
 `;
 
 module.exports = { query_most_recent_create_on_date };
