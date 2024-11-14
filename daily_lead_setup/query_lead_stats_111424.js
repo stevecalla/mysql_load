7e557e6a8f09994850fcc081062fffd3d5322fe1
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2024-11-14 10:43:08 -0700

    Update query to include dates

--- daily_lead_setup/query_lead_stats_111424.js
+++ daily_lead_setup/query_lead_stats_111424.js
@@ -1,13 +1,22 @@
 function query_lead_stats() {
     return `
         -- #4) LEADS BY CREATED DATE, BY COUNTRY, BY SOURCE
-        -- SELECT DISTINCT(renting_in_country), COUNT(*) FROM leads_master GROUP BY 1;
-
         SELECT 
             DATE_FORMAT(lm.created_on, '%Y-%m-%d') AS created_on, -- in GST, not in UST so no timezone conversion needed
             lm.renting_in_country,
             ls.source_name,
-            COUNT(*) AS count_leads
+            COUNT(*) AS count_leads,
+            -- CURRENT DATE / TIME GST
+            now() AS now_utc,
+            DATE_FORMAT(DATE_ADD(NOW(), INTERVAL 4 HOUR), '%Y-%m-%d %H:%i:%s') AS created_at_gst,
+            -- Max created_on for all records (without per-grouping)
+            (SELECT 
+                DATE_FORMAT(MAX(created_on), '%Y-%m-%d %H:%i:%s') 
+            FROM leads_master 
+            WHERE created_on >= CURRENT_DATE() - INTERVAL 5 DAY 
+            AND created_on < CURRENT_DATE() + INTERVAL 1 DAY
+            ) AS max_created_on
+
         FROM leads_master AS lm
             LEFT JOIN lead_sources AS ls ON lm.lead_source_id = ls.id
         WHERE 
