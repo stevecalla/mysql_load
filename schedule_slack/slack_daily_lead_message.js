22eb6318ecbb18f3e37b103805547500500ed0fc
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2024-11-16 10:25:06 -0700

    Modify slack message

--- schedule_slack/slack_daily_lead_message.js
+++ schedule_slack/slack_daily_lead_message.js
@@ -5,59 +5,75 @@ const { getFormattedDateAmPm } = require('../utilities/getCurrentDate');
 const { lead_data } = require('../daily_lead_setup/seed_data');
 const { format_lead_data } = require('../daily_lead_setup/utility_group_data');
 
+
+function generateLeadSummary(outputText, segment) {
+  // Destructure the values from the output object
+  const {
+    yesterday_leads,
+    today_leads,
+    yesterday_booking_cancelled,
+    yesterday_booking_confirmed,
+    yesterday_booking_total,
+    today_booking_cancelled,
+    today_booking_confirmed,
+    today_booking_total,
+    yesterday_booking_conversion,
+    today_booking_conversion,
+  } = outputText;
+
+  // Generate the lead summary based on the segment
+  let leadSummary = '';
+  
+  if (segment === 'uae_country') {
+    leadSummary = `LEADS - UAE ONLY\n` +
+      `${yesterday_leads}, Confirmed Bookings ${yesterday_booking_confirmed}, Conversion ${yesterday_booking_conversion}\n` +
+      `${today_leads}, Confirmed Bookings ${today_booking_confirmed}, Conversion ${today_booking_conversion}\n--------------`;
+  } else if (segment === 'uae_source') {
+    leadSummary = `SOURCE - UAE ONLY\n` +
+      `${yesterday_leads}\n${yesterday_booking_conversion} (conversion)\n` +
+      `${today_leads}\n${today_booking_conversion} (conversion)\n--------------`;
+  } else if (segment === 'all_countries') {
+    leadSummary = `LEADS - ALL COUNTRIES\n` +
+      `${yesterday_leads}\n${yesterday_booking_conversion} (conversion)\n` +
+      `${today_leads}\n${today_booking_conversion} (conversion)\nUNK: Unknown = country blank\n--------------`;
+  } else if (segment === 'all_source') {
+    leadSummary = `SOURCE - ALL COUNTRIES\n` +
+      `${yesterday_leads}\n${yesterday_booking_conversion} (conversion)\n` +
+      `${today_leads}\n${today_booking_conversion} (conversion)`;
+  }
+
+  return leadSummary;
+}
+
 async function create_daily_lead_slack_message(data) {
   const { all_countries_output_text, all_source_output_text, uae_only_country_output_text, uae_only_source_output_text } = await format_lead_data(data);
 
-  // AVAILABLE FIELDS
-  // yesterday_leads,
-  // today_leads,
-  // yesterday_booking_cancelled,
-  // yesterday_booking_confirmed,
-  // yesterday_booking_total,
-  // today_booking_cancelled,
-  // today_booking_confirmed,
-  // today_booking_total,
-  // yesterday_booking_conversion,
-  // today_booking_conversion,
+  const lead_uae_country = generateLeadSummary(uae_only_country_output_text, 'uae_country');
+  const lead_uae_source = generateLeadSummary(uae_only_source_output_text, 'uae_source');
+  const lead_all_countries = generateLeadSummary(all_countries_output_text, 'all_countries');
+  const lead_all_source = generateLeadSummary(all_source_output_text, 'all_source');
 
-  const { 
-      yesterday_leads,
-      today_leads,
-      today_booking_conversion: today_conversion_all_countries,
-  } = all_countries_output_text;
-  
-  const {
-    yesterday_leads: yesterday_leads_source,
-    today_leads: today_leads_source,
-    today_booking_conversion: today_conversion_all_source,
-  } = all_source_output_text
+  let { queried_at_message, most_recent_date_message } = await date_info(data);
 
-  const {
-    yesterday_leads: yesterday_leads_uae_country,
-    today_leads: today_leads_uae_country,
-    yesterday_booking_confirmed: yesterday_bookings_confirmed_uae_only,
-    today_booking_confirmed: today_bookings_confirmed_uae_only,
-    yesterday_booking_conversion: yesterday_conversion_uae_only,
-    today_booking_conversion: today_conversion_uae_only
+  // FINAL MESSAGE
+  // \n${most_recent_date_message} // took this out because the most recent lead at looks wrong
+  const slackMessage = 
+    `\n**************\n` +
+    `LEADS DATA\n${queried_at_message}` +
+    `\n--------------\n` +
+    `${lead_uae_country}\n` +
+    `${lead_uae_source}\n` +
+    `${lead_all_countries}\n` +
+    `${lead_all_source}\n` +
+    `--------------\n` +
+    `Response Time: IN PROGRESS\n` +
+    `Conversion - Same Day: IN PROGRESS\n` +
+    `**************\n`
+  ;
 
-  } = uae_only_country_output_text;
+  console.log(slackMessage);
 
-  const {
-    yesterday_leads: yesterday_leads_uae_source,
-    today_leads: today_leads_uae_source,
-    today_booking_conversion: today_conversion_uae_only_source,
-  } = uae_only_source_output_text
-
-    let { queried_at_message, most_recent_date_message } = await date_info(data);
-
-    // FINAL MESSAGE
-    // \n${most_recent_date_message} // took this out because the most recent lead at looks wrong
-    const slackMessage = 
-      `\n**************\nLEADS DATA\n${queried_at_message}\n--------------\nLEADS - UAE ONLY\n${yesterday_leads_uae_country}, Confirmed Bookings ${yesterday_bookings_confirmed_uae_only}, Conversion ${yesterday_conversion_uae_only}\n${today_leads_uae_country}, Confirmed Bookings ${today_bookings_confirmed_uae_only}, Conversion ${today_conversion_uae_only}\n--------------\nSOURCE - UAE ONLY\n${yesterday_leads_uae_source}\n${today_leads_uae_source}\n${today_conversion_uae_only_source} (conversion)\n--------------\nLEADS - All COUNTRIES\n${yesterday_leads}\n${today_leads}\n${today_conversion_all_countries} (conversion)\nUNK: Unknown = country blank\n--------------\nSOURCE - ALL COUNTRIES\n${yesterday_leads_source}\n${today_leads_source}\n${today_conversion_all_source} (conversion)\n--------------\nResponse Time: TBD\nConversion - Same Day: TBD\n**************\n`;
-
-    console.log(slackMessage);
-
-    return slackMessage;
+  return slackMessage;
 }
 
 // CREATE DATE INFO
@@ -73,17 +89,17 @@ async function date_info(data) {
 }
 
 // used to test
-async function main() {
-  // TEST WITH SEED DATA
-  create_daily_lead_slack_message(lead_data);
+// async function main() {
+//   // TEST WITH SEED DATA
+//   create_daily_lead_slack_message(lead_data);
 
-  // TEST VIA THE API
-  // let data = await execute_get_daily_lead_data();
+//   // TEST VIA THE API
+//   // let data = await execute_get_daily_lead_data();
 
-  // create_daily_lead_slack_message(data);
-}
+//   // create_daily_lead_slack_message(data);
+// }
 
-main();
+// main();
 
 module.exports = {
   create_daily_lead_slack_message,
