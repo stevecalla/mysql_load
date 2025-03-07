34560003c31d1061807ef4a9eaf1131a7f50790b
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-03-07 06:03:58 -0700

    Replace comma with pipe

--- load_bigquery/move_data_to_bigquery/query_booking_keyMetrics_pacing.js
+++ load_bigquery/move_data_to_bigquery/query_booking_keyMetrics_pacing.js
@@ -296,13 +296,15 @@ const profileQuery = `
         user_is_verified, 
 
         -- all_nps_scores,
-        CONCAT('"', all_nps_scores, '"') AS all_nps_scores,
+        -- CONCAT('"', all_nps_scores, '"') AS all_nps_scores,
+        CONCAT('"', REPLACE(all_nps_scores, ',', '|'), '"') AS all_nps_scores
 
         most_recent_nps_score,
 		most_recent_nps_comment,
 
         -- all_booking_ids,
-        CONCAT('"', all_booking_ids, '"') AS all_booking_ids,
+        -- CONCAT('"', all_booking_ids, '"') AS all_booking_ids,
+        CONCAT('"', REPLACE(all_nps_scores, ',', '|'), '"') AS all_nps_scores
 
         booking_count_extended,
         
