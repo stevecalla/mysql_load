eb9f08066220d5e88c2fd1fceb229f905385cba7
Steve Calla (he/him) <72281855+stevecalla@users.noreply.github.com>
2025-04-10 20:31:22 -0600

    Adjust daily booking data query v4 to include booking type

--- daily_booking_data/step_1_sql_get_daily_booking_data.js
+++ daily_booking_data/step_1_sql_get_daily_booking_data.js
@@ -8,7 +8,7 @@ const sshClient = new Client();
 
 const { forwardConfig, dbConfig, sshConfig, dbConfigProduction, sshConfigProduction  } = require('../utilities/config');
 
-const { query_booking_count_today_v3_all_bookings } = require('./query_booking_count_today_v3_all_bookings');
+const { query_booking_count_today_v4_all_bookings } = require('./query_booking_count_today_v4_all_bookings');
 
 // Function to create a Promise for managing the SSH connection and MySQL queries
 function createSSHConnection(is_development_pool = true) {
@@ -57,7 +57,8 @@ async function execute_query_get_daily_booking_data(pool) {
         const startTime = performance.now();
 
         // const query = query_booking_count_today_v2(); // only non cancelled
-        const query = query_booking_count_today_v3_all_bookings();
+        // const query = query_booking_count_today_v3_all_bookings(); // query prior to including cancel / not canceled categories
+        const query = query_booking_count_today_v4_all_bookings();
         // console.log(query);
 
         pool.query(query, (queryError, results) => {
@@ -74,7 +75,7 @@ async function execute_query_get_daily_booking_data(pool) {
 
                 console.log(`Query results length: ${results.length}, Elapsed Time: ${elapsedTime} sec`);
 
-                // console.table(results);
+                console.table(results);
                 // console.log(results);
 
                 resolve(results);
